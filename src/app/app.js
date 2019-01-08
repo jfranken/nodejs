696a42128572c47732b8876cfc1466c78e0baad7
Jakob Frankenbach <jakob.frankenbach@sovanta.com>
2019-01-08 15:04:51 +0100

    change mongodb url

--- src/app/app.js
+++ src/app/app.js
@@ -7,7 +7,7 @@ app.get('/', function (req, res) {
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 
-    mongoose.connect('mongodb://127.0.0.1:27017/test_db').then(() => {
+    mongoose.connect('mongodb://10.0.2.2:27017/test_db').then(() => {
             console.log("Successfully connected to Mongo DB");
         },
         err => {
